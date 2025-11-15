eddbd60de96a34e979ab4fce819ba9bea596c588
Souvik7602 <sm2496444@gmail.com>
2025-11-15 14:33:52 +0530

    update

--- src/main/java/com/spring/employee/Main.java
+++ src/main/java/com/spring/employee/Main.java
@@ -1,18 +1,34 @@
 package com.spring.employee;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.employee.entity.Employee;
+import com.spring.employee.repo.EmployeeDao;
 
 public class Main {
 	public static void main(String[]arg) {
 		
-		ApplicationContext ioc=new ClassPathXmlApplicationContext("application-context.xml");
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		EmployeeDao edao = ioc.getBean("edao", EmployeeDao.class);
 		
-		Employee bean=ioc.getBean("e2",Employee.class);
+		Employee emp1 = new Employee(55, "Nakul","Male", 96000);
+		edao.saveEmployee(emp1);
 		
-		System.out.println(bean);
+		
+//    	edao.updateEmployee(emp1);
+//    	System.out.println("updated successfully");
+//    	edao.deleteEmployee(7);
+//    	edao.deleteEmployee(9);
+//
+//		List<Employee> allEmp=edao.getAllEmp();
+////		
+//		for (Employee employee : allEmp) {
+//			System.out.println(employee);
+//		}
+		System.out.println("Get successfully");
 	}
 
 }
--- src/main/java/com/spring/employee/entity/Employee.java
+++ src/main/java/com/spring/employee/entity/Employee.java
@@ -2,23 +2,30 @@ package com.spring.employee.entity;
 
 public class Employee {
 	private int id;
-	private String name;
-	private String gender;
+	private String name,gender;
 	private int salary;
-	private Address address;
 	
-	public Employee(int id, String name, String gender, int salary, Address address) {
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(int id, String name, String gender, int salary) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.salary = salary;
-		this.address = address;
 	}
-	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
+
+	public String getGender() {
+		return gender;
 	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -31,29 +38,17 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
 	public int getSalary() {
 		return salary;
 	}
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-	
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + ", address="
-				+ address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
 	}
+	
 	
 }
